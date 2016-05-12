e3552df4e72cce6105263fca9f4af614c883741f
Gee <iamgeorgespears@Georges-iMac.local>
2016-05-12 14:34:11 -0500

    Created a rails application and added status updates

---
+++ config/initializers/assets.rb
@@ -0,0 +1,25 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css.erb, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( ionicons.min.css )
+Rails.application.config.assets.precompile += %w( animate.min.css )
+Rails.application.config.assets.precompile += %w( vegas.min.css )
+Rails.application.config.assets.precompile += %w( csoon.css )
+Rails.application.config.assets.precompile += %w( jquery.easing.min.js )
+Rails.application.config.assets.precompile += %w( jquery.countdown.min.js )
+Rails.application.config.assets.precompile += %w( device.min.js )
+Rails.application.config.assets.precompile += %w( wow.min.js )
+Rails.application.config.assets.precompile += %w( smoothscroll.js )
+Rails.application.config.assets.precompile += %w( form.js )
+Rails.application.config.assets.precompile += %w( jquery.placeholder.min.js )
+Rails.application.config.assets.precompile += %w( ionicons.min.css )
